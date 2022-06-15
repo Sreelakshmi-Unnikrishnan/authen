2774017938c8f2a55e2bc067a5ac0e8eee154232
sreelakshmi <sreelakshmialeesha@gmail.com>
2022-06-15 16:37:32 +0530

    password reset implemented

--- socialauth/urls.py
+++ socialauth/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('' ,  home  , name="home"),
@@ -9,5 +10,10 @@ urlpatterns = [
     path('token' , token_send , name="token_send"),
     path('success' , success , name='success'),
     path('verify/<auth_token>' , verify , name="verify"),
-    path('error' , error_page , name="error")
+    path('error' , error_page , name="error"),
+
+    path('password_reset/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
